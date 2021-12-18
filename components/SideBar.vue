15bf3505ab60a0e6cd9a97678752a12a9c2a9acf
mbahdashych <mbahdashych@cryptovoucher.io>
2021-12-18 22:40:55 +0100

    sidebar fixes

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,11 +1,13 @@
 <template>
   <div class='side-bar'>
-    <nuxt-link to="/" style="text-decoration: none; color: white"><h1>pNb</h1></nuxt-link>
+    <nuxt-link to="/" style="text-decoration: none; color: white; text-align: center;"><h1>pNb</h1></nuxt-link>
     <div class='side-bar-menu'>
       <h3>Articles</h3>
-      <h3>Write up's</h3>
+      <h3>Write-ups</h3>
       <h3>Tips</h3>
       <h3>CTF's</h3>
+      <hr>
+      <p>Filter by dates</p>
     </div>
   </div>
 </template>
