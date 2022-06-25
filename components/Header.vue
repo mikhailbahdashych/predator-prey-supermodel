0794fb8f1ecbf35f84de3507358cb748e7d45643
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-25 10:39:46 +0200

    header style fixes

--- components/Header.vue
+++ components/Header.vue
@@ -3,14 +3,26 @@
     <div class="inner-header">
       <div class="header-content">
         <div class="header-content">
-          <nuxt-link to="/" class='nuxt-link'><h3>pNb</h3></nuxt-link>
-          <nuxt-link to="/" class='nuxt-link'><h3>FORUM</h3></nuxt-link>
-          <nuxt-link to="/" class='nuxt-link'><h3>Q&A</h3></nuxt-link>
-          <nuxt-link to="/" class='nuxt-link'><h3>BLOG</h3></nuxt-link>
+          <nuxt-link to="/" class='nuxt-link'>
+            <h3 class="header-links">pNb</h3>
+          </nuxt-link>
+          <nuxt-link to="/" class='nuxt-link'>
+            <h3 class="header-links">FORUM</h3>
+          </nuxt-link>
+          <nuxt-link to="/" class='nuxt-link'>
+            <h3 class="header-links">Q&A</h3>
+          </nuxt-link>
+          <nuxt-link to="/" class='nuxt-link'>
+            <h3 class="header-links">BLOG</h3>
+          </nuxt-link>
         </div>
         <div class="header-content">
-          <div class="header-button"><Button :label="'SIGN IN'" :additional-class="'transparent'" /></div>
-          <div class="header-button"><Button :label="'SIGN UP'" /></div>
+          <div class="header-button">
+            <Button :label="'SIGN IN'" :additional-class="'transparent'" />
+          </div>
+          <div class="header-button">
+            <Button :label="'SIGN UP'" />
+          </div>
         </div>
       </div>
     </div>
