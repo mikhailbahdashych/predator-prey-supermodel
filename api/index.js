b515e4b4a1eadef10214a76d2a30f621b81055ea
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-26 10:13:18 +0200

    api refactor

--- api/index.js
+++ api/index.js
@@ -10,32 +10,12 @@ const api = axios.create({
   }
 })
 
-export const getSelectedReleases = async q => {
-  const { data } = await api.get(`g-s-r/${q}`)
+export const signIn = async payload => {
+  const { data } = await api.post('s-i', payload)
   return data
 }
 
-export const getLatestReleases = async (q) => {
-  const { data } =  await api.get(`g-l-r/${q}`)
-  return data
-}
-
-export const getPostById = async id => {
-  const { data } = await api.get(`g-p/${id}`)
-  return data
-}
-
-export const getPostsByCategory = async d => {
-  const { data } = await api.get(`g-p-b-c/${d.category}/${d.from}/${d.to}`)
-  return data
-}
-
-export const search = async payload => {
-  const { data } = await api.post(`search`, payload)
-  return data
-}
-
-export const sendEmail = async payload => {
-  const { data } = await api.post('/s-e', payload)
+export const signUp = async payload => {
+  const { data } = await api.post('s-u', payload)
   return data
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -11,62 +11,22 @@ const api = axios.create({
 
 const router = Router()
 
-router.get(`/g-s-r/:q`, async (req, res) => {
+router.post('/s-i', async (req, res) => {
   try {
-    const { q } = req.params
-    const data = await api.get(`/get-selected-releases/${q}`)
-    res.json(data.data)
+    const { data } = await api.post('/sign-in', req.body)
+    res.json(data)
   } catch (e) {
-    console.log(e)
+    throw new Error(e)
   }
-})
+});
 
-router.get(`/g-l-r/:q`, async (req, res) => {
+router.post('/s-u', async (req, res) => {
   try {
-    const { q } = req.params
-    const data = await api.get(`/get-latest-releases/${q}`)
-    res.json(data.data)
+    const { data } = await api.post('/sign-up', req.body)
+    res.json(data)
   } catch (e) {
-    console.log(e)
+    throw new Error(e)
   }
-})
-
-router.get(`/g-p/:id`, async (req, res) => {
-  try {
-    const { id } = req.params
-    const data = await api.get(`/get-post/${id}`)
-    res.json(data.data)
-  } catch (e) {
-    console.log(e)
-  }
-})
-
-router.get(`/g-p-b-c/:category/:from/:to`, async (req, res) => {
-  try {
-    const { category, from, to } = req.params
-    const data = await api.get(`/get-posts-by-category/${category}/${from}/${to}`)
-    res.json(data.data)
-  } catch (e) {
-    console.log(e)
-  }
-})
-
-router.post(`/search`, async (req, res) => {
-  try {
-    const data = await api.post(`/search`, req.body)
-    res.json(data.data)
-  } catch (e) {
-    console.log(e)
-  }
-})
-
-router.post('/s-e', async (req, res) => {
-  try {
-    const data = await api.post('/send-email', req.body)
-    res.json(data.data)
-  } catch (e) {
-    console.log(e)
-  }
-})
+});
 
 module.exports = router
