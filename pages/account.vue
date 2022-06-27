f499ffb79ec64cfbe41eb779b5c6291252ae2ba1
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-27 18:47:19 +0200

    account page

--- pages/account.vue
+++ pages/account.vue
@@ -1,10 +1,33 @@
 <template>
-  <div></div>
+  <div class='account'>
+    <div class='account-header'>
+      <div class='account-header-avatar' />
+      <div class='account-header-data'>
+        <h1>@username</h1>
+      </div>
+      <div class='account-header-buttons'>
+        <span class='account-header-button'><Button :label="'Edit profile'" :additional-class="'transparent'" /></span>
+        <span class='account-header-button'><Button :label="'Settings'" :additional-class="'transparent'" /></span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Button from '~/components/Button'
 export default {
-  name: "Account"
+  name: "Account",
+  components: {
+    Button
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
 }
 </script>
 
