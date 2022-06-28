01ac274998db6824da18c6eeaa3bfc508b122d7c
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-28 19:11:18 +0200

    basic modal component

---
+++ components/BasicModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="settings-modal-container">
+    <div class="settings-modal-wrapper">
+      <div class="modal-header">
+        <img src="../assets/img/backarrow.svg" alt="Back" @click="$emit('close')">
+        <h3>{{header}}</h3>
+      </div>
+      <p class="modal-description">{{description}}</p>
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BasicModal",
+  props: {
+    header: String,
+    description: String,
+    onClose: Boolean
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/css/components/BasicModal";
+</style>
