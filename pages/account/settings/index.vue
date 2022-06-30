cb7ae0554e614898efdc2452ce5fd4149de41c06
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-30 16:16:39 +0200

    set2fa modal

--- pages/account/settings/index.vue
+++ pages/account/settings/index.vue
@@ -16,7 +16,6 @@
     </div>
 
     <div v-else-if="currentSection === 'Security settings'" class="account-security-content">
-
       <div v-for="setting in securitySettingsOptions" :key="setting.title" :class="`security-item ${setting.danger ? 'danger' : ''}`">
         <div class="security-items">
           <div class="security-items">
@@ -24,10 +23,36 @@
             <p class="font-second danger-text">{{ setting.description }}</p>
           </div>
           <div class="security-items">
-            <Button :label="setting.buttonTitle" :additional-class="`transparent ${setting.danger ? 'danger' : ''}`" />
+            <Button
+              :label="setting.buttonTitle"
+              :additional-class="`transparent ${setting.danger ? 'danger' : ''}`"
+              @click-handler="openModal(setting.title)" />
           </div>
         </div>
       </div>
+
+      <basic-modal
+        v-if="securityShowModal['Set 2FA']"
+        header="Activate 2FA"
+        description="We strongly recommend you to 2FA.
+      This will increase the security of you account.
+      Before it, you should download Google Authenticator application.
+      Once it's done, click the button below to start."
+        @close="closeModal('Set 2FA')"
+      >
+<!--        <Button v-if="!securityTwofa.qr" :label="'Generate 2FA'" :clickon="generate2fa" />-->
+<!--        <img v-if="securityTwofa.qr && ([null, -1, -2].includes(securityTwofa.status))" :src="securityTwofa.qr" alt="2fa">-->
+<!--        <div v-if="securityTwofa.qr && ([null, -1, -2].includes(securityTwofa.status))">-->
+<!--          <InputTwoFa :twofa="securityTwofa.code" @returnTwofa="returnTwofa" :onwhite="true" />-->
+<!--          <Button :label="'Confirm 2FA'" :clickon="set2fa" />-->
+<!--        </div>-->
+<!--        <div v-else-if="securityTwofa.status === 1">-->
+<!--          <p class="paragraph medium on-white-paragraph">2FA set successfully</p>-->
+<!--        </div>-->
+<!--        <div v-if="securityTwofa.status === -1">-->
+<!--          <p class="paragraph medium error">Wrong code!</p>-->
+<!--        </div>-->
+      </basic-modal>
 
     </div>
 
@@ -40,11 +65,13 @@
 
 <script>
 import Button from "~/components/Button";
+import BasicModal from "~/components/BasicModal";
 import {getUserSettings} from "~/api";
 export default {
   name: "Settings",
   components: {
-    Button
+    Button,
+    BasicModal
   },
   data() {
     return {
@@ -61,7 +88,15 @@ export default {
         { title: 'Change password', description: 'You are able to change email only one time.', buttonTitle: 'Change password', danger: false },
         { title: 'Change email', description: 'You are able to change email only one time.', buttonTitle: 'Change email', danger: false },
         { title: 'Close account', description: 'After closing account all information about it will be deleted.', buttonTitle: 'Close', danger: true }
-      ]
+      ],
+      securityShowModal: {
+        'Set 2FA': false,
+        'Close account': false,
+        'Change email': false,
+        'Change password': false,
+        'Disable 2FA': false,
+        twoFa: false,
+      },
     }
   },
   async mounted() {
@@ -77,6 +112,16 @@ export default {
     changeSubpage(item) {
       this.currentSection = item.title
       this.accountHeaderItems.forEach(header => { header.active = item.title === header.title })
+    },
+    openModal(option) {
+      Object.entries(this.securityShowModal).forEach(item => {
+        if (item[0] === option) this.securityShowModal[item[0]] = true
+      })
+    },
+    closeModal(option) {
+      Object.entries(this.securityShowModal).forEach(item => {
+        if (item[0] === option) this.securityShowModal[item[0]] = false
+      })
     }
   }
 }
