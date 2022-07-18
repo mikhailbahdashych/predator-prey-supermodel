4d1e967f04117eea2fa6006497f660a8a4c9bd89
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-07-18 19:07:09 +0200

    updating colors

--- middleware/theme.js
+++ middleware/theme.js
@@ -1,10 +1,10 @@
 export default function() {
   if (process.server) return
   const t = localStorage.getItem('_t')
-  if (!t || t !== '_d' || t !== '_l') {
+  if (!t) {
     localStorage.setItem('_t', '_d')
     document.documentElement.setAttribute('data-theme', 'dark')
   } else {
-    document.documentElement.setAttribute('data-theme', t === '_d' ? 'dark' : 'light')
+    document.documentElement.setAttribute('data-theme', t === '_l' ? 'light' : 'dark')
   }
 }
